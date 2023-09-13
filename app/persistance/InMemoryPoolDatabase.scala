bb37efb25243e0f56da326b2fdee98b98b2d7244
Luyen Vu <vuhailuyen1991@gmail.com>
2023-09-13 18:02:35 +0700

    Use ConcurrentHashMap

--- app/persistance/InMemoryPoolDatabase.scala
+++ app/persistance/InMemoryPoolDatabase.scala
@@ -1,17 +1,19 @@
 package persistance
 
 import scala.collection.mutable
+import java.util.concurrent.ConcurrentHashMap
+import scala.jdk.CollectionConverters.ConcurrentMapHasAsScala
 
 class InMemoryPoolDatabase extends PoolDatabase {
   override def get(id: Long): Option[Seq[Long]] =
     Pools.map.get(id).map(_.toSeq)
 
-  override def add(id: Long, elements: Seq[Long]): Unit = synchronized {
+  override def add(id: Long, elements: Seq[Long]): Unit =
     if (Pools.map.contains(id))
       Pools.map(id) = Pools.map(id).appendedAll(elements)
     else
       Pools.map += (id -> mutable.Seq.empty[Long].appendedAll(elements))
-  }
+
 
   override def truncate(): Unit = Pools.map.clear()
 
@@ -19,5 +21,5 @@ class InMemoryPoolDatabase extends PoolDatabase {
 }
 
 private object Pools {
-  var map = new mutable.HashMap[Long, mutable.Seq[Long]]()
+  var map = new ConcurrentHashMap[Long, mutable.Seq[Long]]().asScala
 }
